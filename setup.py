4afe3a791c4bd74327cf3d1b7036d86d680ecfc4
MRHRTZ <hanifsyauqi61@gmail.com>
2025-07-03 09:08:52 +0700

    fix: change version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='pyooz',
-    version='0.0.8',
+    version='0.0.9',
     description="ooz decompression bindings",
     long_description=long_description,
     long_description_content_type="text/markdown",
